9cfa1be80a1b8cf096644854eaf20d39063af00b
andy112165 <andy112165@163.com>
2019-09-07 14:58:17 +0800

    ConvertUtil

--- common/src/main/java/com/farm/common/utils/ConvertUtil.java
+++ common/src/main/java/com/farm/common/utils/ConvertUtil.java
@@ -5,13 +5,18 @@ import com.alibaba.fastjson.JSONObject;
 import java.util.List;
 
 /**
- * @description:
+ * @description: 对象转换
  * @author: xyc
  * @create: 2019-09-07 14:52
  */
 public class ConvertUtil {
-
-    public static <T> List<T> convert(List list,Class<T> clazz){
-       return JSONObject.parseArray(JSONObject.toJSONString(list), clazz);
+    /**
+     * @param list
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> convert(List list, Class<T> clazz) {
+        return JSONObject.parseArray(JSONObject.toJSONString(list), clazz);
     }
 }
