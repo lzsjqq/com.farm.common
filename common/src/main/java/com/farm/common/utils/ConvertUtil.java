42491f987aceddaffa820f0dbc6740a766134afd
andy112165 <andy112165@163.com>
2019-09-08 15:28:53 +0800

    ConvertUtil

--- common/src/main/java/com/farm/common/utils/ConvertUtil.java
+++ common/src/main/java/com/farm/common/utils/ConvertUtil.java
@@ -19,4 +19,14 @@ public class ConvertUtil {
     public static <T> List<T> convert(List list, Class<T> clazz) {
         return JSONObject.parseArray(JSONObject.toJSONString(list), clazz);
     }
+
+    /**
+     * @param obj
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T convert(Object obj, Class<T> clazz) {
+        return JSONObject.parseObject(JSONObject.toJSONString(obj), clazz);
+    }
 }
